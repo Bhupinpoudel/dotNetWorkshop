21f512fa8a453cf6627c1a1c49c21fb1b6b7a1cd
Bhupinpoudel <bhupin.poudel@gmail.com>
2023-05-25 09:20:22 +0545

    hello

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var list = new List<HomeVm>(){
+            new HomeVm() {Id = 1, Name="One", Address="Addr one"},
+            new HomeVm() {Id = 2, Name="Two", Address="Addr two"}
+        };
+        return View(list);
+    }
+     
+     public IActionResult New()
+     {
+        return View();
+     } 
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
