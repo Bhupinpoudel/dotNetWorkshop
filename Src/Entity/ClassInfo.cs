996675288114dd90a77d486153221f234cd41d06
Bhupinpoudel <bhupin.poudel@gmail.com>
2023-05-26 09:18:10 +0545

    git test

---
+++ Src/Entity/ClassInfo.cs
@@ -0,0 +1,14 @@
+namespace MyApp.Src .Entity
+{
+    public class ClassInfo{
+        public int Id {get;set;}
+
+        public string? Name {get;set;}
+
+        public string? Description {get;set;}
+
+        public int DateTime {get;set;}
+
+
+    }
+}
